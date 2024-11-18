20f36595b616332328b0802c843c63c5ed75a39a
Nicholas St. Germain <nick@cajun.pro>
2024-11-18 10:42:31 -0600

    add suspense boundary

--- src/app/auth/siwe/page.tsx
+++ src/app/auth/siwe/page.tsx
@@ -2,29 +2,29 @@
 import { generatePayload, isLoggedIn, login, logout } from "@/app/auth/siwe/actions";
 import { client, theme, wallets } from "@/components/thirdweb";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 import type { Address } from "thirdweb";
 import { ConnectButton } from "thirdweb/react";
-
-
 
 const SIWEPage = () => {
   const searchParams = useSearchParams();
   return (
     <div className="flex flex-col grow justify-center items-center">
-      <ConnectButton
-        client={client}
-        wallets={wallets}
-        theme={theme}
-        appMetadata={{ name: "Thirdweb", url: "https://static.thirdweb.com" }}
-        connectModal={{ size: "compact" }}
-        auth={{
-          isLoggedIn: (address) => isLoggedIn(address as Address),
-          doLogin: (params) => login(params, searchParams.get("redirectUrl")),
-          getLoginPayload: async ({ address }) => generatePayload(address as Address),
-          doLogout:  () => logout(),
-        }}
-      />
-
+      <Suspense>
+        <ConnectButton
+          client={client}
+          wallets={wallets}
+          theme={theme}
+          appMetadata={{ name: "Thirdweb", url: "https://static.thirdweb.com" }}
+          connectModal={{ size: "compact" }}
+          auth={{
+            isLoggedIn: (address) => isLoggedIn(address as Address),
+            doLogin: (params) => login(params, searchParams.get("redirectUrl")),
+            getLoginPayload: async ({ address }) => generatePayload(address as Address),
+            doLogout: () => logout(),
+          }}
+        />
+      </Suspense>
     </div>
 
   );
