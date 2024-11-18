ddf666d67e0fe8e4db675b8dc8fe77047166aa90
Nicholas St. Germain <nick@cajun.pro>
2024-11-18 09:50:25 -0600

    catch externals

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config: NextConfig) => {
+    config.externals.push("pino-pretty", "lokijs", "encoding", "fs");
+    return config;
+  }
 };
 
 export default nextConfig;
